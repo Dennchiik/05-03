461fd7d08205e7943e3e83ac0ea9e769e3bebf46
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2024-04-11 16:45:31 +0300

    Add files via upload

---
+++ 05,03(2)/05,03(2)/05,03(2).cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+class Wheels {
+protected:
+    int numberOfWheels;
+
+public:
+    Wheels(int numberOfWheels) : numberOfWheels(numberOfWheels) {}
+
+    void printNumberOfWheels() const {
+        std::cout << "Number of wheels: " << numberOfWheels << std::endl;
+    }
+};
+
+class Engine {
+protected:
+    float horsepower;
+
+public:
+    Engine(float horsepower) : horsepower(horsepower) {}
+
+    void printHorsepower() const {
+        std::cout << "Horsepower: " << horsepower << " hp" << std::endl;
+    }
+};
+
+class Doors {
+protected:
+    int numberOfDoors;
+
+public:
+    Doors(int numberOfDoors) : numberOfDoors(numberOfDoors) {}
+
+    void printNumberOfDoors() const {
+        std::cout << "Number of doors: " << numberOfDoors << std::endl;
+    }
+};
+
+class Car : public Wheels, public Engine, public Doors {
+public:
+    Car(int numberOfWheels, float horsepower, int numberOfDoors)
+        : Wheels(numberOfWheels), Engine(horsepower), Doors(numberOfDoors) {}
+
+    void printCarInfo() const {
+        printNumberOfWheels();
+        printHorsepower();
+        printNumberOfDoors();
+    }
+};
+
+int main() {
+    Car car(4, 200.0, 4);
+    car.printCarInfo();
+
+    return 0;
+}
---
+++ 05,03/05,03/05,03.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+class Square {
+protected:
+    float side;
+
+public:
+    Square(float side) : side(side) {}
+
+    float getSide() const {
+        return side;
+    }
+
+    float calculateArea() const {
+        return side * side;
+    }
+};
+
+class Circle {
+protected:
+    float radius;
+
+public:
+    Circle(float radius) : radius(radius) {}
+
+    float getRadius() const {
+        return radius;
+    }
+
+    float calculateArea() const {
+        return 3.14159 * radius * radius;
+    }
+};
+
+class InscribedCircleSquare : public Square, public Circle {
+public:
+    InscribedCircleSquare(float side) : Square(side), Circle(side / 2) {}
+
+    float calculateCircleArea() const {
+        return Circle::calculateArea();
+    }
+
+    float calculateSquareArea() const {
+        return Square::calculateArea();
+    }
+};
+
+int main() {
+    float side;
+    std::cout << "Enter the side length of the square: ";
+    std::cin >> side;
+
+    InscribedCircleSquare shape(side);
+    std::cout << "Area of the square: " << shape.calculateSquareArea() << std::endl;
+    std::cout << "Area of the inscribed circle: " << shape.calculateCircleArea() << std::endl;
+
+    return 0;
+}
